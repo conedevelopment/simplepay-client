5180beac760536a1ad000bb0bacf9759fd75a44e
D. Nagy Gergő <hello@iamgergo.com>
2025-08-01 23:39:00 +0200

    wip

--- src/Client.php
+++ src/Client.php
@@ -5,11 +5,37 @@ namespace Cone\SimplePay;
 class Client
 {
     /**
+     * The client version.
+     */
+    public const VERSION = '1.0.0';
+
+    /**
      * Create a new SimplePay Client instance.
      */
-    public function __construct(protected string $merchantId, protected string $secretKey)
-    {
+    public function __construct(
+        protected string $merchantId,
+        protected string $secretKey,
+        protected bool $sandbox = false
+    ) {
         // $this->config()->setApiKey('X-API-KEY', $apiKey);
+    }
+
+    /**
+     * Sign the given data.
+     */
+    public function sign(string $data): string
+    {
+        return base64_encode(
+            hash_hmac('sha384', $data, $this->secretKey, true)
+        );
+    }
+
+    /**
+     * Validate the signature.
+     */
+    public function validateSignature(string $hash, string $data): bool
+    {
+        return hash_equals($hash, $this->sign($data));
     }
 
     /**
